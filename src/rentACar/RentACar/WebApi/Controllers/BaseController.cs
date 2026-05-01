6367e39d428919aa87e99d459a379c9125ac73ed
Boran Türk <brnbey10@gmail.com>
2026-05-01 14:10:51 +0300

    Add project files.

---
+++ src/rentACar/RentACar/WebApi/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class BaseController : ControllerBase
+{
+    private IMediator? _mediatr;
+    protected IMediator? Mediator => _mediatr??= HttpContext.RequestServices.GetService<IMediator>(); 
+    //protected yaptık çünkü sadece bu controlleri miras alanlar erişebilsin
+
+    //Daha önce MediatR enjekte edilmişse onu döndür
+    //ama edilmemişse IOC ortamına bak ve MediatR karşılığna bak ve onu döndür. Eğer IOC ortamında da yoksa null döndür.
+}
