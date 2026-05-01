f1d461ff8e82453bed06bd6683fd90daa5c1c063
Boran Türk <brnbey10@gmail.com>
2026-05-01 17:21:07 +0300

    Core.Packages's DynamicFilter & Paginate

---
+++ src/corePackages/Core.Packages/Core.Persistence/Repositories/IEntityTimeStamps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories;
+
+public class IEntityTimeStamps
+{
+    DateTime CreatedDate { get; set; }
+    DateTime? UpdatedDate { get; set; }
+    DateTime? DeletedDate { get; set; }
+}
